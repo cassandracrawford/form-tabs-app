86cd665c8b9a13e1c1146cef84f18c317969e3c1
cassandracrawford <crfeliciano1989@gmail.com>
2025-06-13 15:35:32 -0600

    update style

--- components/Login.tsx
+++ components/Login.tsx
@@ -27,25 +27,33 @@ export default function Login({
   return (
     <View style={styles.container}>
       <StatusBar hidden={true} />
-      <Text style={styles.title}>Login to your account.</Text>
+      <Text style={{...styles.title, fontSize: 32}}>Sign in</Text>
+      <Text style={{...styles.title, fontSize: 14, letterSpacing: 4, marginBottom: 40}}>your account</Text>
       <TextInput
         placeholder="Username"
+        placeholderTextColor = 'white'
         value={username}
         onChangeText={setUsername}
         style={styles.input}
       />
-      {usernameError ? <Text>{usernameError}</Text> : null}
+      {usernameError ? <Text style={{color: 'white'}}>{usernameError}</Text> : null}
       <TextInput
         placeholder="Password"
+        placeholderTextColor = 'white'
         value={password}
         onChangeText={setPassword}
         style={styles.input}
         secureTextEntry
       />
-      {passwordError ? <Text>{passwordError}</Text> : null}
-      <Pressable style={styles.button} onPress={onLogin}>
-        <Text style={{ color: '#fff' }}>Sign in</Text>
+      {passwordError ? <Text style={styles.errorMessage}>{passwordError}</Text> : null}
+      <Text style={{color: 'white'}}>Forgot Password?</Text>
+      <Pressable style={({ pressed }) => [
+        styles.button,
+        pressed && { backgroundColor: pressed ? '#f2f2f2' : 'white' },
+        ]} onPress={onLogin}>
+        <Text style={styles.buttonText}>Sign in</Text>
       </Pressable>
+      <Text style={{color: 'white'}}>Do you have an account? Sign up!</Text>
     </View>
   );
 }
@@ -53,28 +61,40 @@ export default function Login({
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#E94C89',
     alignItems: 'center',
     justifyContent: 'center',
   },
   title: {
-    fontSize: 32,
     fontWeight: 'bold',
-    marginBottom: 20,
+    color: 'white',
+    textTransform: 'uppercase',
+    margin: 2,
   },
   input: {
     padding: 10,
-    borderColor: 'black',
-    borderWidth: 1,
+    borderColor: 'white',
+    borderBottomWidth: 1,
+    marginTop: 10,
     marginBottom: 10,
     width: '80%',
+    color: 'white',
   },
   button: {
-    backgroundColor: 'blue',
+    backgroundColor: 'white',
     padding: 10,
     borderRadius: 20,
-    marginTop: 10,
+    marginTop: 30,
     width: '50%',
     alignItems: 'center',
+  },
+  buttonText: {
+    color: '#E94C89',
+    fontWeight: 900,
+    textTransform: 'uppercase',
+    letterSpacing: 2,
+  },
+  errorMessage: {
+    color: 'white',
   },
 });
